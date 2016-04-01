37724afec76226d23348ffb4182b235a50c4bc16
Andrey Bulat <andrii.bulat@gmail.com>
2016-04-01 15:32:26 +0300

    Derived component class from GameObject

--- app/scripts/component.js
+++ app/scripts/component.js
@@ -1,11 +1,31 @@
+var GameObject = require('./game-object');
+
 function Component(name, object) {
-  this._name = name;
+  if (name && typeof name === 'string') {
+    this._name = name;
+  } else {
+    throw new TypeError('Component: name of component must be specified.');
+  }
+  if (object && typeof object === 'object') {
+    this._object = object;
+  } else {
+    throw new TypeError('Component: object to inject component into must be specified.');
+  }
+
   this._controller();
 }
 
+Component.prototype = Object.create(GameObject.prototype);
+Component.prototype.constructor = Component;
+
 $.extend(Component.prototype, {
+  destroy: function () {
+    GameObject.prototype.destroy.apply(this, arguments);
+    delete this._object[this._name];
+  },
   _controller: function () {
-    object[this._name] = this;
+    GameObject.prototype._controller.apply(this, arguments);
+    this._object[this._name] = this;
   }
 });
 
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 var gameManager = require('./game-manager');
 var GameObject = require('./game-object');
+var Component = require('./component');
 
 window.gameManager = gameManager;
-new GameObject();
