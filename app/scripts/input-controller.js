956e2b72f88e082325c19ad8f5ad76f83d06e559
Andrey Bulat <andrii.bulat@gmail.com>
2016-04-03 23:07:18 +0300

    Added key events handling

--- app/scripts/input-controller.js
+++ app/scripts/input-controller.js
@@ -2,6 +2,9 @@ var GameObject = require('./game-object');
 
 function InputController() {
   GameObject.apply(this, arguments);
+
+  this._inputs = [];
+  this._bindEvents();
 }
 
 InputController.prototype = Object.create(GameObject.prototype);
@@ -10,6 +13,15 @@ InputController.prototype.constructor = InputController;
 $.extend(InputController.prototype, {
   update: function () {
     this._update();
+  },
+  _bindEvents: function () {
+    var bindedKeyboardEventHandler = this._keyboardEventHandler.bind(this);
+    window.addEventListener('keydown', bindedKeyboardEventHandler);
+    window.addEventListener('keyup', bindedKeyboardEventHandler);
+  },
+  _keyboardEventHandler: function (e) {
+    e = e || event;
+    this._inputs[e.keyCode || e.which] = e.type === 'keydown';
   }
 });
 
